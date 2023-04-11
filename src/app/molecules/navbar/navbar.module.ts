a3cf5d7f7e745416a360b28cba42b8aeb2adc88c
ibrahimgb <ibrahim.guoual.b@gmail.com>
2023-04-12 00:31:05 +0100

    add navbar

---
+++ src/app/molecules/navbar/navbar.module.ts
@@ -0,0 +1,11 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { NavbarComponent } from './navbar.component';
+
+@NgModule({
+  declarations: [NavbarComponent],
+  imports: [CommonModule],
+  exports: [NavbarComponent],
+  providers: [],
+})
+export class NavbarModule {}
