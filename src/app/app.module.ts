c26733ad99469cb5201f25c7f6e85cdafc0a38e3
ibrahimgb <ibrahim.guoual.b@gmail.com>
2023-04-13 23:18:47 +0100

    clean workspace

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,14 +1,27 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ComingSoonModule } from 'src/app/molecules/coming-soon/coming-soon.module';
+import { FooterModule } from 'src/app/molecules/footer/footer.module';
+import { NavbarModule } from 'src/app/molecules/navbar/navbar.module';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HomepageModule } from './pages/homepage/homepage.module';
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
+import { BlogModule } from './molecules/blog/blog.module';
+import { HomepageModule } from './pages/homepage/homepage.module';
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule, AppRoutingModule, HomepageModule, BrowserAnimationsModule],
+  imports: [
+    BlogModule,
+    FooterModule,
+    ComingSoonModule,
+    NavbarModule,
+    BrowserModule,
+    AppRoutingModule,
+    HomepageModule,
+    BrowserAnimationsModule,
+  ],
   providers: [],
   bootstrap: [AppComponent],
 })
