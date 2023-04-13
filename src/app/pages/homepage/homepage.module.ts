a956cbd5eedd91e7a2620f099b3044411b9d8cc7
ibrahimgb <ibrahim.guoual.b@gmail.com>
2023-04-13 16:19:19 +0100

    home page cleanup

--- src/app/pages/homepage/homepage.module.ts
+++ src/app/pages/homepage/homepage.module.ts
@@ -3,13 +3,22 @@ import { NgModule } from '@angular/core';
 
 import { MatIconModule } from '@angular/material/icon';
 import { NgImageSliderModule } from 'ng-image-slider';
+import { ComingSoonModule } from 'src/app/molecules/coming-soon/coming-soon.module';
+import { FooterModule } from 'src/app/molecules/footer/footer.module';
 import { NavbarModule } from 'src/app/molecules/navbar/navbar.module';
 import { HomepageComponent } from './homepage.component';
 import { HomepageService } from './homepage.service';
 import { ImageSliderComponent } from './molecules/atoms/image-slider/image-slider.component';
 @NgModule({
   declarations: [HomepageComponent, ImageSliderComponent],
-  imports: [CommonModule, NavbarModule, MatIconModule, NgImageSliderModule],
+  imports: [
+    CommonModule,
+    NavbarModule,
+    MatIconModule,
+    NgImageSliderModule,
+    FooterModule,
+    ComingSoonModule,
+  ],
   exports: [HomepageComponent],
   providers: [HomepageService],
 })
