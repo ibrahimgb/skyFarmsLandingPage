1646bd31e00825bc75cf1e0274cab09bdb4f42bf
ibrahimgb <ibrahim.guoual.b@gmail.com>
2023-04-13 17:35:05 +0100

    finish up coming soon page

---
+++ src/app/molecules/coming-soon/coming-soon.module.ts
@@ -0,0 +1,10 @@
+import { CommonModule } from '@angular/common';
+import { NgModule } from '@angular/core';
+import { ComingSoonComponent } from './coming-soon.component';
+
+@NgModule({
+  declarations: [ComingSoonComponent],
+  imports: [CommonModule],
+  exports: [ComingSoonComponent],
+})
+export class ComingSoonModule {}
